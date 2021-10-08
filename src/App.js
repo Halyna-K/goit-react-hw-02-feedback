9c29eb0b7a2b30eda7c6b9daecc0e4ecc4beb266
Halyna-K <hkzlvs@gmail.com>
2021-10-08 17:33:05 +0300

    create react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import coffeeToGo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={coffeeToGo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
